750aa3c8db4228867c825518f95995853dd30b39
ger619 <draker74@gmail.com>
2023-07-05 20:41:02 +0300

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < ActionController::Base
+  before_action :set_rendered_cart
+  before_action :initialize_cart
+
+  def set_rendered_cart
+    @render_cart = true
+  end
+
+  def initialize_cart
+    @cart ||= Cart.find_by(id: session[:cart_id])
+
+    if @cart.nil?
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+
+  end
+end
